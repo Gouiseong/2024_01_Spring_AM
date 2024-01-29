ec25eae0b3a510edb2c8d0e391269e65f24bc090
Gouiseong <rhdmltjd1324@gmail.com>
2024-01-29 13:33:00 +0900

    ArticleService 도입 및 기능 이전

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,21 +1,17 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.vo.Article;
-
 @Controller
 public class UsrHomeController {
 
-	private int id;
-	private String title;
-	private String body;
-	private Article article;
-	private List<Article> articles = new ArrayList<>();
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+
+		return "MAIN!";
+	}
 
 }
