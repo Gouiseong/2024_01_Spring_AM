90be0650801370c948be92304dbe3260420d63c3
Gouiseong <rhdmltjd1324@gmail.com>
2024-02-19 16:04:58 +0900

    댓글 폼 체크

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -97,12 +97,12 @@ public class UsrArticleController {
 
 		List<Reply> replies = replyService.getForPrintReplies(rq.getLoginedMemberId(), "article", id);
 
-		
 		int repliesCount = replies.size();
-
+	
 		
 		model.addAttribute("article", article);
 		model.addAttribute("replies", replies);
+		
 		model.addAttribute("repliesCount", repliesCount);
 		model.addAttribute("isAlreadyAddGoodRp",
 				reactionPointService.isAlreadyAddGoodRp(rq.getLoginedMemberId(), id, "article"));
