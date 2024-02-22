41756d7fd77f270272fb29be57d67ce770f59ce8
Gouiseong <rhdmltjd1324@gmail.com>
2024-02-22 09:31:29 +0900

    댓글 삭제 구현

--- src/main/java/com/example/demo/controller/UsrReplyController.java
+++ src/main/java/com/example/demo/controller/UsrReplyController.java
@@ -41,10 +41,10 @@ public class UsrReplyController {
 		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (Ut.isNullOrEmpty(relTypeCode)) {
-			return Ut.jsHistoryBack("F-1", "내용을 입력해주세요");
+			return Ut.jsHistoryBack("F-1", "relTypeCode을 입력해주세요");
 		}
 		if (Ut.isEmpty(relId)) {
-			return Ut.jsHistoryBack("F-2", "내용을 입력해주세요");
+			return Ut.jsHistoryBack("F-2", "relId을 입력해주세요");
 		}
 		if (Ut.isNullOrEmpty(body)) {
 			return Ut.jsHistoryBack("F-3", "내용을 입력해주세요");
@@ -57,41 +57,33 @@ public class UsrReplyController {
 		return Ut.jsReplace(writeReplyRd.getResultCode(), writeReplyRd.getMsg(), "../article/detail?id=" + relId);
 
 	}
-	
-//	@RequestMapping("/usr/reply/modify")
-//	public String showModify(HttpServletRequest req, Model model, int id) {
-//		Rq rq = (Rq) req.getAttribute("rq");
-//
-//		Article article =replyService.getForPrintReply(rq.getLoginedMemberId(), id);
-//
-//		if (article == null) {
-//			return Ut.jsHistoryBack("F-1", Ut.f("%d번 글은 존재하지 않습니다", id));
-//		}
-//
-//		model.addAttribute("reply", reply);
-//
-//		return "usr/reply/modify";
-//	}
-//
-//	@RequestMapping("/usr/reply/doModify")
-//	@ResponseBody
-//	public String doModify(HttpServletRequest req, int id, String title, String body) {
-//		Rq rq = (Rq) req.getAttribute("rq");
-//
-//		Article article = articleService.getArticle(id);
-//
-//		if (article == null) {
-//			return Ut.jsHistoryBack("F-1", Ut.f("%d번 글은 존재하지 않습니다", id));
-//		}
-//
-//		ResultData loginedMemberCanModifyRd = articleService.userCanModify(rq.getLoginedMemberId(), article);
-//
-//		if (loginedMemberCanModifyRd.isSuccess()) {
-//			articleService.modifyArticle(id, title, body);
-//		}
-//
-//		return Ut.jsReplace(loginedMemberCanModifyRd.getResultCode(), loginedMemberCanModifyRd.getMsg(),
-//				"../article/detail?id=" + id);
-//	}
+
+	@RequestMapping("/usr/reply/doDelete")
+	@ResponseBody
+	public String doDelete(HttpServletRequest req, int id, String replaceUri) {
+		Rq rq = (Rq) req.getAttribute("rq");
+
+		Reply reply = replyService.getReply(id);
+
+		if (reply == null) {
+			return Ut.jsHistoryBack("F-1", Ut.f("%d번 댓글은 존재하지 않습니다", id));
+		}
+
+		ResultData loginedMemberCanDeleteRd = replyService.userCanDelete(rq.getLoginedMemberId(), reply);
+
+		if (loginedMemberCanDeleteRd.isSuccess()) {
+			replyService.deleteReply(id);
+		}
+
+		if (Ut.isNullOrEmpty(replaceUri)) {
+			switch (reply.getRelTypeCode()) {
+			case "article":
+				replaceUri = Ut.f("../article/detail?id=%d", reply.getRelId());
+				break;
+			}
+		}
+
+		return Ut.jsReplace(loginedMemberCanDeleteRd.getResultCode(), loginedMemberCanDeleteRd.getMsg(), replaceUri);
+	}
 
 }
